99f1c8dd555e185bd57705487372285b2bb87acf
Arnaud Guéras <arnaudgs@gmail.com>
2025-04-07 19:19:58 +0200

    fix storybook
    Signed-off-by: Arnaud Guéras <arnaudgs@gmail.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 
 import remarkGfm from "remark-gfm";
-const { mergeConfig } = require("vite");
-const path = require("path");
+import { mergeConfig } from "vite";
+import path from "path";
 
 const config = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -12,32 +12,26 @@ const config = {
     "@storybook/addon-essentials",
     {
       name: "@storybook/addon-docs",
-      options: {
-        mdxPluginOptions: {
-          mdxCompileOptions: {
-            remarkPlugins: [remarkGfm],
-          },
-        },
-      },
+      options: { mdxPluginOptions: { mdxCompileOptions: { remarkPlugins: [remarkGfm] } } },
     },
   ],
-  framework: {
-    name: "@storybook/web-components-vite",
-    options: {},
-  },
+  framework: { name: "@storybook/web-components-vite", options: {} },
   viteFinal: async (config, { configType }) => {
     return mergeConfig(config, {
+      resolve: { alias: { "@": path.resolve(__dirname, "../") } },
       css: {
         preprocessorOptions: {
           scss: {
             silenceDeprecations: ["legacy-js-api"],
             additionalData: `
-            @import "./.storybook/storybook.scss";
+            @use "./storybook.scss";
             `,
           },
         },
       },
+      optimizeDeps: { include: ["storybook-dark-mode"] },
     });
   },
 };
+
 export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,19 +1,15 @@
-import "../static/font-awesomepro/fontawesome.css"; // Adjust the path as needed
+// Import styles
+import "../static/font-awesomepro/fontawesome.css";
 import "../static/font-awesomepro/regular.css";
 import "../static/font-awesomepro/solid.css";
 import "../static/font-awesomepro/brands.css";
 
-import "../themes/xen-project/assets/css/main.scss"; // Adjust the path as needed
+import "../themes/xen-project/assets/css/main.scss";
 
-/** @type { import('@storybook/web-components').Preview } */
 const preview = {
   parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: { matchers: { color: /(background|color)$/i, date: /Date$/ } },
   },
 };
 
