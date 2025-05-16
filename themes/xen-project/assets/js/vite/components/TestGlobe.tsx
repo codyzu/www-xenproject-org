740dda86a21221e232d0eb507b57a47dbe129cc8
Cody Zuschlag <cody.zuschlag@gmail.com>
2025-05-16 16:34:40 +0200

    poc: test globe

---
+++ themes/xen-project/assets/js/vite/components/TestGlobe.tsx
@@ -0,0 +1,242 @@
+import {h, render, type RefObject} from 'preact';
+import {useEffect, useState, useLayoutEffect, useRef} from 'preact/hooks';
+import Globe, {type GlobeMethods} from 'react-globe.gl';
+import useResizeObserver from '@react-hook/resize-observer';
+import clsx from 'clsx';
+import {type ParsedJob} from './HardwareGrid/schema.ts';
+
+const useSize = (target: RefObject<HTMLDivElement>) => {
+  const [size, setSize] = useState<DOMRect>();
+
+  useLayoutEffect(() => {
+    if (target.current === null) {
+      return;
+    }
+
+    setSize(target.current.getBoundingClientRect());
+  }, [target]);
+
+  // Where the magic happens
+  useResizeObserver(target, (entry) => {
+    setSize(entry.contentRect);
+  });
+  return size;
+};
+
+type MarkerData = {
+  readonly lat: number;
+  readonly lng: number;
+  readonly text: string;
+  readonly details: string;
+};
+
+function Marker({text, details}: Pick<MarkerData, 'text' | 'details'>) {
+  return (
+    <div className="uno-bg-gray uno-border-solid uno-border-1 uno-border-brand-fill uno-bg-opacity-60 uno-rounded-xl uno-p-2">
+      <div className="uno-w-10 uno-h-10 i-mdi-cpu-64-bit" />
+      <div className="uno-text-xs uno-font-semibold">{text}</div>
+      <div className="uno-text-xs uno-font-mono">{details}</div>
+    </div>
+  );
+}
+
+type Location = {
+  readonly lat: number;
+  readonly lng: number;
+  readonly name: string;
+  readonly icons: string[];
+};
+
+const sanJose: Location = {lat: 37.3382, lng: -121.8863, name: 'San Jose', icons: ['i-mdi-cpu-64-bit']};
+const grenoble: Location = {lat: 45.1885, lng: 5.7245, name: 'Grenoble', icons: ['i-mdi-cpu-64-bit']};
+const boston: Location = {lat: 42.3601, lng: -71.0589, name: 'Boston', icons: ['i-mdi-cpu-64-bit']};
+const amsterdam: Location = {lat: 52.3676, lng: 4.9041, name: 'Amsterdam', icons: ['i-mdi-cpu-64-bit']};
+
+const cities: Location[] = [sanJose, grenoble, boston, amsterdam];
+
+const combinations = cities.flatMap((start, startIndex) =>
+  cities.filter((end, endIndex) => start !== end && endIndex > startIndex).map((end) => ({start, end})),
+);
+const arcsData = combinations.map(({start, end}) => ({
+  startLat: start.lat,
+  startLng: start.lng,
+  endLat: end.lat,
+  endLng: end.lng,
+  // Color: 'red',
+}));
+
+const N = 30;
+const gData: MarkerData[] = [
+  {
+    ...sanJose,
+    text: 'San Jose',
+    details: `Xen Project number San Jose`,
+  },
+  {
+    ...grenoble,
+    text: 'Grenoble',
+    details: `Xen Project number Grenoble`,
+  },
+  {
+    ...boston,
+    text: 'Boston',
+    details: `Xen Project number Boston`,
+  },
+  {
+    ...amsterdam,
+    text: 'Amsterdam',
+    details: `Xen Project number Amsterdam`,
+  },
+];
+
+const northPole = {lat: 90, lng: 0};
+
+export default function TestGlobe({jobs}: {readonly jobs: Map<string, ParsedJob[]>}) {
+  const globeContainerRef = useRef<HTMLDivElement>(null);
+  const globeContainerSize = useSize(globeContainerRef);
+  const globeRef = useRef<GlobeMethods>();
+
+  useEffect(() => {
+    if (!globeRef.current) {
+      console.warn('Globe ref is not set');
+      return;
+    }
+
+    // GlobeRef.current.controls().addEventListener('end', () => {
+    //   console.log('controls ended');
+    // });
+
+    // globeRef.current.camera().addEventListener('end', () => {
+    //   console.log('camera ended');
+    // });
+
+    globeRef.current.controls().autoRotate = true;
+    globeRef.current.controls().autoRotateSpeed = 1.8;
+
+    // GlobeRef.current.pointOfView({...northPole, altitude: 0.6});
+  }, [globeRef.current]);
+
+  useEffect(() => {
+    if (!globeRef.current) {
+      console.warn('Globe ref is not set');
+      return;
+    }
+
+    if (jobs.size === 0) {
+      console.warn('No jobs found');
+      return;
+    }
+
+    // Let cancel = false;
+
+    // async function rotateCities() {
+    //   while (true) {
+    //     for (const city of cities) {
+    //       if (cancel) {
+    //         return;
+    //       }
+
+    //       // eslint-disable-next-line no-await-in-loop
+    //       await new Promise((resolve) => {
+    //         // GlobeRef.current?.pointOfView?({...city, altitude: 1.8}, 2000);
+    //         globeRef.current?.pointOfView({...city, altitude: 1.8}, 2000);
+    //         setTimeout(() => {
+    //           resolve(true);
+    //         }, 2000);
+    //       });
+    //     }
+    //   }
+    // }
+
+    // void rotateCities();
+
+    console.log('Globe rer set', globeRef.current);
+
+    // Return () => {
+    //   cancel = true;
+    // };
+
+    // Const c = globeRef.current.controls();
+
+    // globeRef.current.controls().addEventListener('end', () => {
+    //   console.log('Animation ended');
+    // });
+
+    // Auto-rotate
+    // globeRef.current.controls().autoRotate = true;
+    // globeRef.current.controls().autoRotateSpeed = 0.8;
+    // globeRef.current.pointOfView({...northPole, altitude: 1});
+    // globeRef.current.pointOfView({...sanJose, altitude: 1.8}, 5000);
+    // GlobeRef.current.controls().
+  }, [jobs]);
+
+  return (
+    <div
+      ref={globeContainerRef}
+      className="uno-w-full uno-h-120 uno-flex uno-flex-col uno-items-center uno-self-center"
+    >
+      {globeContainerSize ? (
+        <Globe
+          ref={globeRef}
+          animateIn={false}
+          globeImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/earth-day.jpg"
+          // GlobeImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/earth-dark.jpg"
+          bumpImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/earth-topology.png"
+          // BackgroundImageUrl="//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png"
+          width={globeContainerSize.width}
+          height={globeContainerSize.height}
+          // HtmlElementsData={gData}
+          // htmlElement={(d) => {
+          //   const data = d as MarkerData;
+          //   const element = document.createElement('div');
+          //   render(<Marker {...data} />, element);
+          //   return element;
+          // }}
+          htmlElementsData={cities}
+          htmlElement={(d) => {
+            const data = d as Location;
+            const element = document.createElement('div');
+            render(
+              <div className="uno-card uno-bg-opacity-70 uno-flex uno-flex-col uno-items-center">
+                {data.icons.map((icon) => (
+                  <div key={icon} className={clsx(icon, 'uno-text-3xl uno-flex-shrink-0')} />
+                ))}
+                <div className="uno-text-xs uno-font-semibold">{data.name}</div>
+              </div>,
+              element,
+            );
+            return element;
+          }}
+          arcsData={arcsData}
+          arcStroke={2}
+          arcAltitudeAutoScale={0.25}
+          arcColor={['#f87171', '#fbbf24', '#4ade80', '#fbbf24', '#f87171']}
+          arcDashGap={0.01}
+          arcDashLength={0.2}
+          arcDashAnimateTime={4000}
+          backgroundColor="#ededed"
+          // LabelsData={cities}
+          // labelText={(l) => {
+          //   return (l as Location).name;
+          // }}
+          // labelSize={2}
+          // labelDotRadius={6}
+          // labelColor={() => 'rgba(255, 255, 255, 0.8)'}
+          // LabelLabel={(l) => {
+          //   const location = l as Location;
+          //   return <div>{location.name}</div>;
+          //   // Return location.icons.map((icon) => (
+          //   //   <div key={icon} className={clsx(icon, 'uno-text-2xl uno-flex-shrink-0')} />
+          //   // ));
+          // }}
+          // OnAnimationEnd={() => {
+          //   console.log('Animation ended');
+          // }}
+          // onZoom={(x) => {
+          //   console.log('Zoom', x);
+          // }}
+        />
+      ) : null}
+    </div>
+  );
+}
